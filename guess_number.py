b00dac8a34ef47bd2867e95ff8afc0a2e856beae
Hardilpreet Kaur <hardilpreetkaur2@gmail.com>
2025-12-28 01:04:39 +0530

    Add number guessing game implementation
    This file contains a simple command-line number guessing game that provides hints to the user based on their guesses.

---
+++ guess_number.py
@@ -0,0 +1,20 @@
+# Number Guessing Game 🎯
+
+A simple Python command-line game where the user tries to guess a randomly generated number.
+
+## How the Game Works
+- The program generates a random number between 1 and 100
+- The user guesses the number
+- The program gives hints: too high or too low
+- The game ends when the correct number is guessed
+
+## Concepts Used
+- Variables
+- Loops
+- Conditional statements
+- Random module
+- User input
+
+## How to Run
+```bash
+python number_guessing_game.py
